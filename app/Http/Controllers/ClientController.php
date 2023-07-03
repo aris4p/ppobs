45869a1bc9e9c181359aff830b464c9361548683
Aris Anggara Putra <arisanggara72@gmail.com>
2023-07-03 21:00:35 +0700

    update curl

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -11,12 +11,12 @@ class ClientController extends Controller
     public function index()
     {
         $product = Product::all();
-
-
+        
+        
         return view('index', compact('product'));
     }
-
-   
+    
+    
     
     public function produk($id)
     {
@@ -26,7 +26,6 @@ class ClientController extends Controller
         
         $curl = curl_init();
         
-
         curl_setopt_array($curl, array(
             CURLOPT_FRESH_CONNECT  => true,
             CURLOPT_URL            => 'https://tripay.co.id/api-sandbox/merchant/payment-channel',
@@ -36,57 +35,57 @@ class ClientController extends Controller
             CURLOPT_FAILONERROR    => false,
             CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
         ));
-
-
-        $response = curl_exec($curl);
-        $error = curl_error($curl);
-
-        curl_close($curl);
-
-        $result = json_decode($response)->data;
         
         $response = curl_exec($curl);
         $error = curl_error($curl);
         
         curl_close($curl);
         
+        if ($error) {
+            // Handle the cURL error appropriately
+            echo "cURL Error: " . $error;
+        } else {
+            $result = json_decode($response)->data;
+            // Process the response data as needed
+        }
         
         
-        $result = json_decode($response)->data;
-        // dd($result);
-
+        
+        
+      
+        
         $produk = Product::where('id', $id)->first();
         // dd($produk);
         return view('produk.produk',
         compact('produk','result'));
     }
-
-
-
+    
+    
+    
     
     public function pembayaran(Request $request)
     {
-
+        
         // dd($request->all());
-
+        
         $product = Product::find($request->id);
         // dd($product->harga);
         // dd(intval($product->harga));
-
-       
+        
+        
         $apiKey       = env('TRIPAY_API_KEY');
         $privateKey   = env('TRIPAY_PRIVATE_KEY');
         $merchantCode = 'T21486';
         $merchantRef  = 'INV6969';
         $amount       = intval($request->product);
         // dd($amount);
-
+        
         // // dd(intval($amount));
         //  $signature = hash_hmac('sha256', $merchantCode.$merchantRef.$amount, $privateKey);
         // dd($signature);
-       
+        
         // 6d753d4e4225df642336e9458f46ddd9de2454c248e9bd2e9ffd3626bfaf22c3
-    //   107f7744b5c7cb1d96a6b24f2c0dddf55ac7021971074110d116aa18746b6abd
+        //   107f7744b5c7cb1d96a6b24f2c0dddf55ac7021971074110d116aa18746b6abd
         
         $data = [
             'method'         => $request->pembayaran,
@@ -105,13 +104,13 @@ class ClientController extends Controller
                     'image_url'   => 'https://tokokamu.com/product/nama-produk-1.jpg',
                 ],
                 // [
-                //     'sku'         => 'FB-07',
-                //     'name'        => 'Nama Produk 2',
-                //     'price'       => 500000,
-                //     'quantity'    => 1,
-                //     'product_url' => 'https://tokokamu.com/product/nama-produk-2',
-                //     'image_url'   => 'https://tokokamu.com/product/nama-produk-2.jpg',
-                //     ]
+                    //     'sku'         => 'FB-07',
+                    //     'name'        => 'Nama Produk 2',
+                    //     'price'       => 500000,
+                    //     'quantity'    => 1,
+                    //     'product_url' => 'https://tokokamu.com/product/nama-produk-2',
+                    //     'image_url'   => 'https://tokokamu.com/product/nama-produk-2.jpg',
+                    //     ]
                 ],
                 'return_url'   => 'https://domainanda.com/redirect',
                 'expired_time' => (time() + (48 * 60 * 60)), // 48 jam
@@ -137,33 +136,34 @@ class ClientController extends Controller
             $error = curl_error($curl);
             
             curl_close($curl);
-           
-         $responses  = json_decode($response, true);
-        //   return $response ?: $error; 
+            
+            $responses  = json_decode($response, true);
+            //   return $response ?: $error; 
             // if ($responses['success'] === true){
-
-            // return $response;
-            // }
+                
+                // return $response;
+                // }
+                
+                // return redirect()->back();
+                
+                Transaction::create([
+                    'reference' => $responses['data']['reference'],
+                    'amount' => $responses['data']['amount'],
+                    'status' => $responses['data']['status'],
+                ]);
+                
+                
+            }
             
-            // return redirect()->back();
             
-            Transaction::create([
-                'reference' => $responses['data']['reference'],
-                'amount' => $responses['data']['amount'],
-                'status' => $responses['data']['status'],
-            ]);
-       
-            
+            public function pembayaran_detail(Request $request)
+            {
+                $req = $request->all();
+                dd($req);
+                return view ('produk.pembayaran_detail', compact('req'));
+            }
         }
-
-
-    public function pembayaran_detail(Request $request)
-    {
-        $req = $request->all();
-        dd($req);
-        return view ('produk.pembayaran_detail', compact('req'));
-    }
-}
-
-    
-
+        
+        
+        
+        
